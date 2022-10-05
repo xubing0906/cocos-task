2860438d31441177a9129a5d919b82f1535dd901
xubing <58895777@qq.com>
2022-10-05 20:33:58 +0800

    probe

--- TestProject/assets/renderTexture/renderTextureTest.ts
+++ TestProject/assets/renderTexture/renderTextureTest.ts
@@ -1,4 +1,4 @@
-import {ReflectionProbeManager,_decorator, Component, Node, RenderTexture, director, Director, Camera, native, renderer, Label, Root, Sprite, SpriteFrame, resources, assetManager, ImageAsset, Texture2D, Light, Color, Quat, UITransform, Vec2, Size, Material } from 'cc';
+import {ReflectionProbeManager,_decorator, Component, Node, RenderTexture, director, Director, Camera, native, renderer, Label, Root, Sprite, SpriteFrame, resources, assetManager, ImageAsset, Texture2D, Light, Color, Quat, UITransform, Vec2, Size, Material, MeshRenderer } from 'cc';
 import { HTML5, NATIVE, PREVIEW } from 'cc/env';
 const { ccclass, property } = _decorator;
 @ccclass('renderTextureTest')
@@ -11,6 +11,11 @@ export class renderTextureTest extends Component {
     private _width = 0;
     private _height = 0;
     start() {
+        const meshrender = this.node.getComponent(MeshRenderer);
+        if (meshrender!==null) {
+            console.log(meshrender.name);
+            console.log('meshrender============');
+        }
     }
     update(deltaTime: number) {
     }
@@ -25,45 +30,45 @@ export class renderTextureTest extends Component {
     }
     onFocusInEditor()
     {
-        let caps =  director.root.device.capabilities;
-        if (caps.clipSpaceMinZ == -1) {
-        }
-        if (caps.clipSpaceSignY == -1) {
-        }
-        this._rt = new RenderTexture();
-        this._rt.reset({ width: this.camera.camera.width, height: this.camera.camera.width });
-        this.camera.targetTexture = this._rt;
-        this._width = this.camera.camera.width;
-        this._height = this.camera.camera.height;
+        // let caps =  director.root.device.capabilities;
+        // if (caps.clipSpaceMinZ == -1) {
+        // }
+        // if (caps.clipSpaceSignY == -1) {
+        // }
+        // this._rt = new RenderTexture();
+        // this._rt.reset({ width: this.camera.camera.width, height: this.camera.camera.width });
+        // this.camera.targetTexture = this._rt;
+        // this._width = this.camera.camera.width;
+        // this._height = this.camera.camera.height;
 
 
-        this.scheduleOnce(function () {
-            let pixelData = this._rt.readPixels();
-            let caps =  director.root.device.capabilities;
-            if (caps.clipSpaceMinZ == -1) {
-                pixelData = this.flipImageX(pixelData);
-            }
-            if (caps.clipSpaceSignY == -1) {
-                pixelData = this.flipImageY(pixelData);
-            }
-            pixelData = this.flipImageY(pixelData);
-            if (this.material) {
-                this.material.setProperty("mainTexture", this._rt, 0);
-            }
-            console.log(this._width);
-            console.log(this._height);
-            const path = 'D:/cocosProject/cocos-task/TestProject/assets/renderTexture/';
-            const fileName = 'testRendercc.png';
-            let fullpath = path + fileName;
-            console.log("full path========"+fullpath);
+        // this.scheduleOnce(function () {
+        //     let pixelData = this._rt.readPixels();
+        //     let caps =  director.root.device.capabilities;
+        //     if (caps.clipSpaceMinZ == -1) {
+        //         pixelData = this.flipImageX(pixelData);
+        //     }
+        //     if (caps.clipSpaceSignY == -1) {
+        //         pixelData = this.flipImageY(pixelData);
+        //     }
+        //     pixelData = this.flipImageY(pixelData);
+        //     if (this.material) {
+        //         this.material.setProperty("mainTexture", this._rt, 0);
+        //     }
+        //     console.log(this._width);
+        //     console.log(this._height);
+        //     const path = 'D:/cocosProject/cocos-task/TestProject/assets/renderTexture/';
+        //     const fileName = 'testRendercc.png';
+        //     let fullpath = path + fileName;
+        //     console.log("full path========"+fullpath);
 
-            // this.saveDataToImage(pixelData, this._width, this._height, fullpath, (res) => {
-            //     console.log(res);
-            // });
-            //this.renderCamera(this.camera);
-            ReflectionProbeManager.probeManager.addProbe(this.camera);
+        //     // this.saveDataToImage(pixelData, this._width, this._height, fullpath, (res) => {
+        //     //     console.log(res);
+        //     // });
+        //     //this.renderCamera(this.camera);
+        //     //ReflectionProbeManager.probeManager.addProbe(this.camera);
             
-        });
+        // });
     }
     flipImageX(data:Uint8Array){
         //图片数组纵向翻转
